840bc4bd111939db1cb7bfed3c0929da5bc5cb0d
soemilia <73540464+soemilia@users.noreply.github.com>
2023-09-05 18:16:44 -0300

    Update aapc_incidence.R

--- IBD/aapc_incidence.R
+++ IBD/aapc_incidence.R
@@ -1,28 +1,25 @@
-#função poisson
+#Função para calculo da incidência (Neg binomial)
 
-calcular_aapc_poisson <- function(casos, populacao, anos) {
+calcular_aapc_incidence <- function(casos, populacao, anos) {
         
-        modelo <- glm.nb(casos ~ anos + offset(log(populacao)))
+        modelo <- glm.nb(as.numeric(casos) ~ anos + offset(log(populacao)))
         
         #coef. do modelo
         coef_ano <- coef(modelo)[2]
-        graus_liberdade_residual <- df.residual(modelo)
-        confianca <- 0.975
-        graus_liberdade <- graus_liberdade_residual - 2  
-        tval <- qt(1 - confianca / 2, df = graus_liberdade)
+        confianca <- 0.05
+         
+        zval <- qnorm(1 - confianca / 2)
         SEm <- coef(summary(modelo))[2, "Std. Error"]
         
         # Intervalo de confiança
-        ic_upper <- (exp(coef_ano + (tval * SEm)) - 1) * 100
-        ic_lower <- (exp(coef_ano - (tval * SEm)) - 1) * 100
+        ic_upper <- (exp(coef_ano + (zval * SEm)) - 1) * 100
+        ic_lower <- (exp(coef_ano - (zval * SEm)) - 1) * 100
         
         #AAPC
         aapc <- (exp(coef_ano) - 1) * 100
         
         #Valor de p
-        valor_absoluto_m_SEm <- abs(coef_ano / SEm)
-        graus_liberdade_residual <- df.residual(modelo)
-        valor_p <- 2 * pt(-valor_absoluto_m_SEm, df = graus_liberdade_residual)
+        valor_p <- coef(summary(modelo))[2, "Pr(>|z|)"]
         
         resultado <- list(
                 p_valor = valor_p,
